e32ca4f892f522bf8a9600e907ebb1df494a484a
wyidi <ydhan1024@gmail.com>
2026-06-22 17:26:35 +0900

    refactor: add empty nixus library

--- flake-module.nix
+++ flake-module.nix
@@ -1,8 +1,13 @@
-nixus: { lib, ... }: {
+nixus: { lib, ... }: let
+  nixus-lib-paths = lib.filter (n: lib.strings.hasSuffix ".nix" n) (lib.filesystem.listFilesRecursive ./lib);
+
+  nixus-lib = lib.fix (self: builtins.foldl' (a: b: a // b) {} (map (path: import path { inherit self; inherit lib; } ) nixus-lib-paths));
+in {
   imports = [( nixus.inputs.import-tree ./modules )];
 
   _module.args = {
     inherit nixus;
+    inherit nixus-lib;
   };
 
   _module.args.lib = lib.extend (final: prev: {
