8125eb1844728e55613990dc2fec33d5ad47699b
wyidi <ydhan1024@gmail.com>
2026-07-04 21:53:18 +0900

    wip: some test cases

--- tests/graph.nix
+++ tests/graph.nix
@@ -1,9 +1,33 @@
-{ ... }: {
+{ nixus-lib, ... }: let
+  inherit (nixus-lib) linearize TODO;
+in {
   flake.tests = {
+    TopologicalSort.testCaseA = let
+      graph = { nodeA = []; }; # graph is represented as adjacency list
+    in {
+      expr     = linearize graph;
+      expected = [ [ "nodeA" ] ]; # linearized strongly connected components
+    };
 
-    testTest = {
-      expr = 1;
-      expected = 1;
+    TopologicalSort.testCaseB = let
+      graph = { nodeA = []; nodeB = []; nodeC = []; }; 
+    in {
+      expr     = linearize graph;
+      expected = [ [ "nodeA" "nodeB" "nodeC" ] ]; 
+    };
+
+    TopologicalSort.testCaseC = let
+      graph = { }; 
+    in {
+      expr     = linearize graph;
+      expected = [ ]; 
+    };
+
+    TopologicalSort.testCaseD = let
+      graph = { nodeA = []; nodeB = [ "nodeA" ]; }; 
+    in {
+      expr     = linearize graph;
+      expected = [ [ "nodeA" ] [ "nodeB" ] ]; 
     };
 
   };
