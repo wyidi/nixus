5fbc6a117fb14e54487b25b8d98fce3560bb7b15
wyidi <ydhan1024@gmail.com>
2026-07-04 20:15:40 +0900

    wip: fixing error from nix-unit

--- check-module.nix
+++ check-module.nix
@@ -3,4 +3,11 @@ nixus: { ... }: {
     (nixus.inputs.import-tree ./tests)
     (nixus.inputs.nix-unit.modules.flake.default)
   ];
+
+  perSystem = { ... }: {
+    nix-unit.inputs = {
+      # NOTE: a `nixpkgs-lib` follows rule is currently required
+      inherit (nixus.inputs) nixpkgs flake-parts nix-unit;
+    };
+  };
 }
