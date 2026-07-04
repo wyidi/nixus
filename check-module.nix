7a2affbb134324a66de344ba131756c39297f606
wyidi <ydhan1024@gmail.com>
2026-07-04 20:52:40 +0900

    wip: fixed nix-unit issue completely

--- check-module.nix
+++ check-module.nix
@@ -6,9 +6,11 @@ nixus: { ... }: {
 
   perSystem = { ... }: {
     # required: flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-
+    # https://flake.parts/options/nix-unit.html#opt-perSystem.nix-unit.inputs
     nix-unit.inputs = {
-      inherit (nixus.inputs) nixpkgs flake-parts import-tree colmena nix-unit;
+      # import-tree is required for test to be evaluated so it is included here
+      # input that test does not depend on need not to be included here (such as colmena)
+      inherit (nixus.inputs) nixpkgs flake-parts import-tree nix-unit;
     };
   };
 }
