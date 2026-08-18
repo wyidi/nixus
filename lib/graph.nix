d8829444e136c3b77df888da5b35dd6f7538932b
wyidi <ydhan1024@gmail.com>
2026-08-19 03:07:38 +0900

    feat: add topological sort
    Add tail-recursive implementation of topological sort.

--- lib/graph.nix
+++ lib/graph.nix
@@ -9,6 +9,44 @@
     # uniqueStrings is not needed since duplicate will be removed at lst2
     zipAttrsWith (_: concatLists) lst3;
 
+  linearize = graph: let
+    vertices = attrNames graph;
+    # A reversed graph is used to find sinks
+    graph'   = reverse   graph;
+    # A sink is a node that no other nodes depends on it
+    sinks    = filter (x: graph'.${x} == []) vertices;
+
+    # Outputs list of minimum heights of each node in DAG
+    # TODO: add `ancestors` parameter to detect back edge (no cycle detection currently)
+    getLevels = levels: queue:
+      if queue == [] then
+        levels
+      else let e = head queue; in
+        if hasAttr e levels then
+          getLevels levels (drop 1 queue)
+        else let parents = graph.${e}; in
+          if parents == [] then
+            getLevels (levels // { ${e} = 1; }) (drop 1 queue)
+          else let xs = filter (x: !(hasAttr x levels)) parents; in
+            if xs == [] then let lst = map (x: levels.${x}) parents; in
+              # height is max heigh of parent nodes + 1
+              getLevels (levels // { ${e} = 1 + (foldl max (head lst) lst); }) (drop 1 queue)
+            else
+              getLevels levels (xs ++ queue);
+
+    levels  = getLevels {} sinks;
+
+    levels' = reverseAttrs (mapAttrs (_: level: [level]) levels);
+    # mapping integer to singleton list is needed because reverAttrs assumes that value is list
+  in {
+    inherit  graph';
+    inherit  levels;
+    inherit levels';
+
+    result = map (level: sort (a: b: a < b) level.value) (sort (x: y: (toIntBase10 x.name) < (toIntBase10 y.name)) (attrsToList levels'));
+  };
+
 in {
   inherit reverse;
+  inherit linearize;
 }
--- tests/graph.nix
+++ tests/graph.nix
@@ -1,5 +1,5 @@
 { nixus-lib, ... }: let
-  inherit (nixus-lib) reverse;
+  inherit (nixus-lib) reverse linearize;
 in {
 
   flake.tests.ReverseGraph = {
@@ -31,6 +31,52 @@ in {
       expected = { nodeA = ["nodeB"]; nodeB = []; };
     };
 
+  };
+
+
+  flake.tests.TopologicalSort = {
+    testCase1 = let
+      graph = { nodeA = []; }; # graph is represented as adjacency list
+    in {
+      expr     = (linearize graph).result or null;
+      expected = [ [ "nodeA" ] ]; # linearized strongly connected components
+    };
+
+    testCase2 = let
+      graph = { nodeA = []; nodeB = []; nodeC = []; };
+    in {
+      expr     = (linearize graph).result or null;
+      expected = [ [ "nodeA" "nodeB" "nodeC" ] ];
+    };
+
+    testCase3 = let
+      graph = { };
+    in {
+      expr     = (linearize graph).result or null;
+      expected = [ ];
+    };
+
+    testCase4 = let
+      graph = { nodeA = []; nodeB = [ "nodeA" ]; };
+    in {
+      expr     = (linearize graph).result or null;
+      expected = [ [ "nodeA" ] [ "nodeB" ] ];
+    };
+
+    testCase5 = let
+      graph = {
+        nodeA = [];
+        nodeB = [ "nodeA" ];
+
+        nodeC = [];
+        nodeD = [ "nodeC" ];
+        nodeE = [ "nodeD" ];
+        nodeF = [ "nodeB" "nodeE" ];
+      };
+    in {
+      expr     = (linearize graph).result or null;
+      expected = [ [ "nodeA" "nodeC" ] [ "nodeB" "nodeD" ] [ "nodeE" ] [ "nodeF" ] ];
+    };
   };
 
 }
