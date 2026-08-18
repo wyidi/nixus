cf8e065c2065d2a7c9e43bfcdec004c6a5ed79d2
wyidi <ydhan1024@gmail.com>
2026-08-19 02:27:08 +0900

    refactor: modularize nixus library injection

--- flake-module.nix
+++ flake-module.nix
@@ -1,16 +1,12 @@
-nixus: { lib, ... }: let
-  nixus-lib-paths = lib.filter (n: lib.strings.hasSuffix ".nix" n) (lib.filesystem.listFilesRecursive ./lib);
-
-  nixus-lib = lib.fix (self: builtins.foldl' (a: b: a // b) {} (map (path: import path { inherit self; inherit lib; } ) nixus-lib-paths));
+nixus: { flake-parts-lib, ... }: let
+  assetModule = flake-parts-lib.importApply ./asset-module.nix nixus;
 in {
-  imports = [( nixus.inputs.import-tree ./modules )];
+  imports = [
+    (nixus.inputs.import-tree ./modules)
+    assetModule
+  ];
 
   _module.args = {
     inherit nixus;
-    inherit nixus-lib;
   };
-
-  _module.args.lib = lib.extend (final: prev: {
-    TODO = builtins.throw "TODO";
-  });
 }
