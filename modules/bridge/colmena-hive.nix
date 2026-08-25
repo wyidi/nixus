5b84f738f9785b04f3e8a51f51f71ced3321537e
wyidi <ydhan1024@gmail.com>
2026-08-25 14:14:06 +0900

    fix(colmena): allow configuring specialArgs

--- modules/bridge/colmena-hive.nix
+++ modules/bridge/colmena-hive.nix
@@ -20,6 +20,10 @@ in {
         type = types.attrsOf types.raw;
       };
 
+      options.specialArgs = mkOption {
+        type = types.attrsOf types.raw;
+      };
+
       options.machinesFile = mkOption {
         type = types.path;
       };
