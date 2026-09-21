284762302be1319e18f398d0f825e0efba9bbed1
wyidi <ydhan1024@gmail.com>
2026-09-22 01:50:38 +0900

    wip: spawn nix eval process

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -15,6 +15,37 @@ enum Commands {
 }
 
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
+
+    use std::process::Command;
+    use std::io::{self, Write};
+
+    match &cli.command {
+        Commands::Plan {} => {
+            let output = Command::new("nix").arg("eval")
+                .arg("--impure")
+                .arg("--json")
+                .arg("--expr")
+                .arg(
+                    r#"let
+                      flake = builtins.getFlake (toString ./.);
+                    in
+                      flake.nixus.${builtins.currentSystem}.API.TopoSort { }
+                "#)
+                .output()?;
+
+            println!("status: {}", output.status);
+            io::stdout().write_all(&output.stdout)?;
+            io::stderr().write_all(&output.stderr)?;
+
+            Ok(())
+        }
+        Commands::Apply {} => {
+            Ok(())
+        }
+        Commands::Destroy {} => {
+            Ok(())
+        }
+    }
 }
