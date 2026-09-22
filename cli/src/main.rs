6a32d3255b8466d202850a92723dea0196ff8b9f
wyidi <ydhan1024@gmail.com>
2026-09-22 23:53:10 +0900

    wip: refactor

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -14,32 +14,36 @@ enum Commands {
     Destroy {},
 }
 
+fn topological_sort() {
+    use std::process::Command;
+
+    let output = Command::new("nix")
+        .arg("eval")
+        .arg("--impure")
+        .arg("--json")
+        .arg("--expr")
+        .arg(r#"
+        let
+            flake = builtins.getFlake (toString ./.);
+        in
+            flake.nixus.${builtins.currentSystem}.API.TopoSort { }
+        "#)
+        .output()
+        .expect("Failed to call nixus TopoSort API");
+
+    println!("status: {}", output.status);
+
+    use std::io::{self, Write};
+    io::stdout().write_all(&output.stdout).expect("Failed to write at stdout");
+    io::stderr().write_all(&output.stderr).expect("Failed to write at stderr");
+}
 
 fn main() {
     let cli = Cli::parse();
 
-    use std::process::Command;
-
     match &cli.command {
         Commands::Plan {} => {
-            let output = Command::new("nix").arg("eval")
-                .arg("--impure")
-                .arg("--json")
-                .arg("--expr")
-                .arg(r#"
-                let
-                    flake = builtins.getFlake (toString ./.);
-                in
-                    flake.nixus.${builtins.currentSystem}.API.TopoSort { }
-                "#)
-                .output()
-                .expect("Failed to spawn nix process");
-
-            println!("status: {}", output.status);
-
-            use std::io::{self, Write};
-            io::stdout().write_all(&output.stdout).expect("Failed to stdout");
-            io::stderr().write_all(&output.stderr).expect("Failed to stderr");
+            
         }
         Commands::Apply {} => {
 
